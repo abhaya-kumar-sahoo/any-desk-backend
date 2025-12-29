f047b051784a8e920d947ff24b44ff0b6955bf01
Abhaya <61579704+abhaya-kumar-sahoo@users.noreply.github.com>
2025-12-29 15:23:50 +0530

    sd

--- server/server.js
+++ server/server.js
@@ -60,7 +60,7 @@ io.on("connection", (socket) => {
     });
 });
 
-const PORT = 1000;
+const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Signaling server running on port ${PORT}`);
 });
